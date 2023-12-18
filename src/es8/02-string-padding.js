14343e8bef2d3f24189ea5cb5510875e1a710957
Fabián Huertas <miloh9318@hotmail.com>
2023-12-18 11:14:25 -0500

    ES8: string padding y trailing commas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,4 @@
+const string = 'Hola';
+
+console.log(string.padStart(8, 'Holi'));
+console.log(string.padEnd(5, '_'));
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,4 @@
+const age = [24, 34, 30, 19];
+
+console.log(age);
+console.log(age.length);
